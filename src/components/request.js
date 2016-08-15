7ad98a027eac0b84962987d62635350af28872a9
Noah Schutte <noahschutte@gmail.com>
2016-08-15 17:09:31 -0400

    change fetch requests to local host

--- src/components/request.js
+++ src/components/request.js
@@ -10,10 +10,29 @@ export default class Request extends Component {
       errorMessage: " "
     };
   }
+  componentWillMount() {
+    fetch(`http://localhost:3000/requests/1`)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if (responseJson.errorMessage === 'No current requests.') {
+        this.setState({errorMessage: responseJson.errorMessage})
+      } else {
+        this.setState({errorMessage: ' '})
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Successfully create Request show page.</Text>
+        <Text>
+          Successfully create Request show page.
+        </Text>
+        <Text>
+          {this.state.errorMessage}
+        </Text>
       </View>
     )
   }
